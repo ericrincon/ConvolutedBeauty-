00ee7f1defc9a6ed1a34f02cd0adc8321519d589
Eric Rincon <eric.rincon147@gmail.com>
2018-10-21 22:44:51 -0500

    Added new dense block

--- convolutedbeauty/models/architectures.py
+++ convolutedbeauty/models/architectures.py
@@ -1,4 +1,41 @@
 import torchvision
 import torch.nn as nn
 
-c
+from collections import OrderedDict
+from convolutedbeauty.models.block import SqueezeExcitationBlock, DenseBlock
+
+
+def build_se_dense():
+    nn.Sequential()
+
+
+class DenseNet(nn.Module):
+    """
+    Simple implementation of DenseNet
+
+    link: https://arxiv.org/abs/1608.06993
+    """
+
+    def __init__(self, block, nb_channels, init_kernel_size):
+        super(DenseNet, self).__init__()
+
+        self.conv_counter = 0
+
+        self.first_convolution(nn.Sequential(OrderedDict([
+            (self._get_conv_name(), nn.Conv2d(3, )),
+            ("batchnorm0", nn.BatchNorm2d()),
+            ()
+        ])))
+
+    def _get_conv_name(self):
+        name = "conv{}".format(self.conv_counter)
+
+        self.conv_counter += 1
+
+        return name
+
+    def _build_dense_block(self, nb_blocks, in_channels, out_channels):
+        blocks = [(self._get_conv_name(), self.block(in_channels, out_channels))
+                  for _ in range(nb_blocks)]
+
+        return nn.Sequential(OrderedDict(blocks))
--- convolutedbeauty/models/block.py
+++ convolutedbeauty/models/block.py
@@ -1,6 +1,8 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+from collections import OrderedDict
+
 
 class SqueezeExcitationBlock(nn.Module):
     """
@@ -10,14 +12,15 @@ class SqueezeExcitationBlock(nn.Module):
     link: https://arxiv.org/pdf/1709.01507.pdf
     """
 
-    def __init__(self, input_size, in_channels, out_channels, kernel_size, reduction_ratio):
+    def __init__(self, input_size, in_channels, out_channels, reduction_ratio, name="squeezeexictation"):
         super(SqueezeExcitationBlock, self).__init__()
 
+        self.name = name
         self.global_pooling = nn.AvgPool2d(input_size)
 
         # where fc refers to fully connected
-        self.fc_1 = nn.Conv2d(in_channels, out_channels/reduction_ratio, input_size)
-        self.fc_2 = nn.Conv2d(out_channels/reduction_ratio, in_channels, input_size)
+        self.fc_1 = nn.Conv2d(in_channels, out_channels / reduction_ratio, input_size)
+        self.fc_2 = nn.Conv2d(out_channels / reduction_ratio, in_channels, input_size)
 
     def forward(self, x):
         """
@@ -31,5 +34,27 @@ class SqueezeExcitationBlock(nn.Module):
         output = F.relu(output)  # 1 x 1 x C/r
         output = self.fc_2(output)  # 1 x 1 x C
         output = F.sigmoid(output)  # 1 x 1 x C
+
+        return output
+
+
+def dense_conv(in_channels, out_channels, kernel_size):
+    return nn.Sequential(OrderedDict([
+        ("bn", nn.BatchNorm2d()),
+        ("conv", nn.Conv2d(in_channels, out_channels, kernel_size)),
+        ("relu", nn.ReLU()),
+    ]))
+
+
+class DenseBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DenseBlock, self).__init__()
+
+        self.conv1 = dense_conv(in_channels, out_channels)
+        self.conv2 = dense_conv(in_channels, out_channels)
+
+    def forward(self, x):
+        output = self.conv1(x)
+        output = self.conv2(output)
 
         return output
